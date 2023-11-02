205ca21cd3179bbe73c2a02a9dcf7856d56ca115
Marco Peixoto <marcoaspeixoto@gmail.com>
2023-11-02 10:57:01 -0300

    chore(talk): import txt route

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :talks do
+    collection do
+      post 'import_from_txt'
+    end
+  end
 end
