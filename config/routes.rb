e3061494d3581266116dca31f39f48f5b64d3646
Marco Peixoto <marcoaspeixoto@gmail.com>
2023-11-02 12:34:08 -0300

    chore: route to organize conference

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :talks do
     collection do
       post 'import_from_txt'
+      get 'organize_conference', to: 'conference#organize'
     end
   end
 end
