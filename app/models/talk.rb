1a120cc6732f894c0eb642682338af7ac3ff78b8
Marco Peixoto <marcoaspeixoto@gmail.com>
2023-11-04 17:47:13 -0300

    feat(talk): adding day column
    feat(talk): adding day column

--- app/models/talk.rb
+++ app/models/talk.rb
@@ -1,2 +1,3 @@
 class Talk < ApplicationRecord
+  enum day: { dia_a: 'Dia A', dia_b: 'Dia B' }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_11_01_192837) do
+ActiveRecord::Schema.define(version: 2023_11_04_200451) do
 
   create_table "talks", force: :cascade do |t|
     t.string "name"
     t.integer "duration"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "day"
   end
 
 end
