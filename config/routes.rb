fcadc256a6e1abeaa490b97d554cc5bbc4bd3f9f
Marco Peixoto <marcoaspeixoto@gmail.com>
2023-11-04 18:56:28 -0300

    chore: import route correction

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   resources :talks do
     collection do
       post 'import_from_txt'
-      get 'organize_conference', to: 'conference#organize'
     end
   end
+
+  post 'talks/import', to: 'talks#import' # Esta rota para importação não precisa estar aninhada
+  get 'organize_conference', to: 'conference#organize' # Certifique-se de que a rota de organização da conferência esteja correta
 end
+
