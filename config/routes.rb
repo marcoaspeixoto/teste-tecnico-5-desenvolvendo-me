26e959a5951de90cbb5e4c15571c2a9bd6154b0e
Marco Peixoto <marcoaspeixoto@gmail.com>
2023-11-06 17:56:59 -0300

    feat(talk): index route

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
   resources :talks do
     collection do
+      get 'index'
       post 'import_from_txt'
     end
   end
 
   post 'talks/import', to: 'talks#import' # Esta rota para importação não precisa estar aninhada
-  get 'organize_conference', to: 'conference#organize' # Certifique-se de que a rota de organização da conferência esteja correta
 end
-
