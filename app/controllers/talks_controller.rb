a1719ccb315a6fe98eb381a823aa39ae896ba9d0
Marco Peixoto <marcoaspeixoto@gmail.com>
2023-11-28 22:10:43 -0300

    feat(talk): organization of morning and afternoon lectures on day A

--- app/controllers/talks_controller.rb
+++ app/controllers/talks_controller.rb
@@ -50,38 +50,32 @@ class TalksController < ApplicationController
         end
 
         if current_time_day_a < Time.new(Time.now.year, Time.now.month, Time.now.day, 12, 0)
-          organized_talks_day_a << { name: name, duration: duration_minutes, day: 'Dia A', start_time: current_time_day_a }
-          current_time_day_a += duration_minutes * 60
-        elsif current_time_day_b < Time.new(Time.now.year, Time.now.month, Time.now.day, 12, 0)
-          organized_talks_day_b << { name: name, duration: duration_minutes, day: 'Dia B', start_time: current_time_day_b }
-          current_time_day_b += duration_minutes * 60
+          if current_time_day_a + duration_minutes * 60 <= Time.new(Time.now.year, Time.now.month, Time.now.day, 13, 0)
+            organized_talks_day_a << { name: name, duration: duration_minutes, day: 'Dia A', start_time: current_time_day_a }
+            current_time_day_a += duration_minutes * 60
+          else
+            # Adiciona o almoço
+            organized_talks_day_a << { name: 'Almoço', duration: 60, day: 'Dia A', start_time: current_time_day_a }
+            current_time_day_a = Time.new(Time.now.year, Time.now.month, Time.now.day, 13, 0) # Inicia a tarde às 13h
+          end
+        elsif current_time_day_a < Time.new(Time.now.year, Time.now.month, Time.now.day, 17, 0)
+          # Verifica se a próxima palestra cabe no tempo restante do dia atual antes das 17h
+          if current_time_day_a + duration_minutes * 60 <= Time.new(Time.now.year, Time.now.month, Time.now.day, 17, 0)
+            organized_talks_day_a << { name: name, duration: duration_minutes, day: 'Dia A', start_time: current_time_day_a }
+            current_time_day_a += duration_minutes * 60
+          else
+            # Adiciona o evento de networking
+            organized_talks_day_a << { name: 'Evento de Networking', duration: 60, day: 'Dia A', start_time: current_time_day_a }
+            current_time_day_a = Time.new(Time.now.year, Time.now.month, Time.now.day, 17, 0) # Inicia o networking entre 16h e 17h
+          end
         end
+
       else
         # Lidar com linhas que não correspondem ao padrão esperado
         # Por exemplo, você pode registrar essas linhas em um arquivo de log ou tratá-las de outra forma adequada
       end
     end
 
-    # Adiciona o evento de almoço no meio do dia
-    lunch_start_time_day_a = Time.new(Time.now.year, Time.now.month, Time.now.day, 12, 0)
-    lunch_start_time_day_b = Time.new(Time.now.year, Time.now.month, Time.now.day, 12, 0)
-
-    organized_talks_day_a << { name: 'Almoço', duration: 60, day: 'Dia A', start_time: lunch_start_time_day_a }
-    organized_talks_day_b << { name: 'Almoço', duration: 60, day: 'Dia B', start_time: lunch_start_time_day_b }
-
-    current_time_day_a = lunch_start_time_day_a + 60 * 60 # Continue após o almoço no Dia A
-    current_time_day_b = lunch_start_time_day_b + 60 * 60 # Continue após o almoço no Dia B
-
-    # Adiciona o evento de networking no final da tarde do Dia A
-    networking_start_time_day_a = [current_time_day_a, Time.new(Time.now.year, Time.now.month, Time.now.day, 16, 0)].max
-
-    organized_talks_day_a << { name: 'Evento de Networking', duration: 60, day: 'Dia A', start_time: networking_start_time_day_a }
-
-    # Adiciona o evento de networking no final da tarde do Dia B
-    networking_start_time_day_b = [current_time_day_b, Time.new(Time.now.year, Time.now.month, Time.now.day, 16, 0)].max
-
-    organized_talks_day_b << { name: 'Evento de Networking', duration: 60, day: 'Dia B', start_time: networking_start_time_day_b }
-
-    organized_talks_day_a + organized_talks_day_b
+    organized_talks_day_a
   end
 end
