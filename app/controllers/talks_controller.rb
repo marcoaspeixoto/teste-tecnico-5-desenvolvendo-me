5f6d5e5b7245ce5e7b9d303f0abee83345f73536
Marco Peixoto <marcoaspeixoto@gmail.com>
2023-12-02 18:26:51 -0300

    feat(talk): organization with sound days

--- app/controllers/talks_controller.rb
+++ app/controllers/talks_controller.rb
@@ -13,13 +13,15 @@ class TalksController < ApplicationController
 
     Talk.destroy_all # Limpa todas as palestras existentes
 
-    organized_talks.each do |talk_data|
-      Talk.create(
-        name: talk_data[:name],
-        duration: talk_data[:duration],
-        day: talk_data[:day],
-        start_time: talk_data[:start_time]
-      )
+    organized_talks.each do |day, talks|
+      talks.each do |talk_data|
+        Talk.create(
+          name: talk_data[:name],
+          duration: talk_data[:duration],
+          day: talk_data[:day],
+          start_time: talk_data[:start_time]
+        )
+      end
     end
 
     render json: { message: 'Palestras importadas e organizadas com sucesso' }
@@ -28,15 +30,13 @@ class TalksController < ApplicationController
   private
 
   def organize_talks(file_data)
-    organized_talks_day_a = []
-    organized_talks_day_b = []
-    current_time_day_a = Time.new(Time.now.year, Time.now.month, Time.now.day, 9, 0) # Começa às 9h no Dia A
-    current_time_day_b = Time.new(Time.now.year, Time.now.month, Time.now.day, 9, 0) # Começa às 9h no Dia B
+    organized_talks_by_day = initialize_organized_talks_by_day
+    current_time_by_day = initialize_current_times
 
     file_data.each_line.with_index do |line, index|
-      last_line = index == file_data.each_line.count - 1 # armazena true caso seja a última linha
+      last_line = index == file_data.each_line.count - 1
 
-      next if line.strip.empty? # Ignora linhas em branco
+      next if line.strip.empty?
 
       match_data = line.match(/^(.+?) (\d+min|lightning)$/)
 
@@ -49,44 +49,38 @@ class TalksController < ApplicationController
           duration_minutes = duration.to_i
         end
 
-        # Adiciona as palestras no dia A
-        if current_time_day_a <= Time.new(Time.now.year, Time.now.month, Time.now.day, 12, 0)
-          if current_time_day_a + duration_minutes * 60 <= Time.new(Time.now.year, Time.now.month, Time.now.day, 12, 0)
-            organized_talks_day_a << { name: name, duration: duration_minutes, day: 'Dia A', start_time: current_time_day_a }
-            current_time_day_a += duration_minutes * 60
-          else
-            # Adiciona o almoço
-            organized_talks_day_a << { name: 'Almoço', duration: 60, day: 'Dia A',
-                                       start_time: Time.new(Time.now.year, Time.now.month, Time.now.day, 12, 0) }
-            current_time_day_a = Time.new(Time.now.year, Time.now.month, Time.now.day, 13, 0) # Inicia a tarde às 13h
-            # Adiciona a palestra após o almoço
-            organized_talks_day_a << { name: name, duration: duration_minutes, day: 'Dia A', start_time: current_time_day_a}
-            current_time_day_a += duration_minutes * 60
-          end
-        elsif current_time_day_a < Time.new(Time.now.year, Time.now.month, Time.now.day, 17, 0)
-          # Verifica se a próxima palestra cabe no tempo restante do dia atual antes das 17h
-          if current_time_day_a + duration_minutes * 60 <= Time.new(Time.now.year, Time.now.month, Time.now.day, 17, 0)
-            organized_talks_day_a << { name: name, duration: duration_minutes, day: 'Dia A', start_time: current_time_day_a }
-            current_time_day_a += duration_minutes * 60
-            if last_line
-              organized_talks_day_a << { name: 'Evento de Networking', duration: 60, day: 'Dia A', start_time: current_time_day_a }
-            end
-          else
-            # Adiciona o evento de networking
-            organized_talks_day_a << { name: 'Evento de Networking', duration: 60, day: 'Dia A', start_time: current_time_day_a }
-            # Adiciona palestra no dia seguinte
-            current_time_day_a = Time.new(Time.now.year, Time.now.month, Time.now.day, 9, 0) # Começa às 9h no Dia A
-            organized_talks_day_a << { name: name, duration: duration_minutes, day: 'Dia A', start_time: current_time_day_a }
-            current_time_day_a += duration_minutes * 60
-          end
+        day = current_time_by_day.keys.find { |d| current_time_by_day[d][:time] <= Time.new(Time.now.year, Time.now.month, Time.now.day, 12, 0) }
+
+        if current_time_by_day[day][:time] + duration_minutes * 60 <= Time.new(Time.now.year, Time.now.month, Time.now.day, 12, 0)
+          organized_talks_by_day[day] << { name: name, duration: duration_minutes, day: "Dia #{day}", start_time: current_time_by_day[day][:time] }
+          current_time_by_day[day][:time] += duration_minutes * 60
+        else
+          organized_talks_by_day[day] << { name: 'Almoço', duration: 60, day: "Dia #{day}", start_time: Time.new(Time.now.year, Time.now.month, Time.now.day, 12, 0) }
+          current_time_by_day[day][:time] = Time.new(Time.now.year, Time.now.month, Time.now.day, 13, 0)
+          organized_talks_by_day[day] << { name: name, duration: duration_minutes, day: "Dia #{day}", start_time: current_time_by_day[day][:time] }
+          current_time_by_day[day][:time] += duration_minutes * 60
         end
 
+        # Adiciona evento de networking se for a última linha
+        if last_line
+          day = current_time_by_day.keys.find { |d| current_time_by_day[d][:time] < Time.new(Time.now.year, Time.now.month, Time.now.day, 17, 0) }
+          organized_talks_by_day[day] << { name: 'Evento de Networking', duration: 60, day: "Dia #{day}", start_time: current_time_by_day[day][:time] }
+        end
       else
         # Lidar com linhas que não correspondem ao padrão esperado
         # Por exemplo, você pode registrar essas linhas em um arquivo de log ou tratá-las de outra forma adequada
       end
     end
 
-    organized_talks_day_a
+    organized_talks_by_day.values.flatten
   end
+
+  def initialize_current_times
+    { time: Time.new(Time.now.year, Time.now.month, Time.now.day, 9, 0), letter: 'A' }
+  end
+
+  def next_day(day)
+    (day.ord + 1).chr
+  end
+
 end
