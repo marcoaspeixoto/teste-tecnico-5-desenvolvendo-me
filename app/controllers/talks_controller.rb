d05b9e7d3c578e76148a2708c6f68a34cd8a4c4b
Marco Peixoto <marcoaspeixoto@gmail.com>
2023-12-04 16:56:35 -0300

    feat(talk): organization of lectures with dynamically created days

--- app/controllers/talks_controller.rb
+++ app/controllers/talks_controller.rb
@@ -13,15 +13,13 @@ class TalksController < ApplicationController
 
     Talk.destroy_all # Limpa todas as palestras existentes
 
-    organized_talks.each do |day, talks|
-      talks.each do |talk_data|
-        Talk.create(
-          name: talk_data[:name],
-          duration: talk_data[:duration],
-          day: talk_data[:day],
-          start_time: talk_data[:start_time]
-        )
-      end
+    organized_talks.each do |talk_data|
+      Talk.create(
+        name: talk_data[:name],
+        duration: talk_data[:duration],
+        day: talk_data[:day],
+        start_time: talk_data[:start_time]
+      )
     end
 
     render json: { message: 'Palestras importadas e organizadas com sucesso' }
@@ -30,8 +28,9 @@ class TalksController < ApplicationController
   private
 
   def organize_talks(file_data)
-    organized_talks_by_day = initialize_organized_talks_by_day
-    current_time_by_day = initialize_current_times
+    organized_talks = []
+    current_time = Time.new(Time.now.year, Time.now.month, Time.now.day, 9, 0)
+    day = 'A'
 
     file_data.each_line.with_index do |line, index|
       last_line = index == file_data.each_line.count - 1
@@ -49,38 +48,44 @@ class TalksController < ApplicationController
           duration_minutes = duration.to_i
         end
 
-        day = current_time_by_day.keys.find { |d| current_time_by_day[d][:time] <= Time.new(Time.now.year, Time.now.month, Time.now.day, 12, 0) }
-
-        if current_time_by_day[day][:time] + duration_minutes * 60 <= Time.new(Time.now.year, Time.now.month, Time.now.day, 12, 0)
-          organized_talks_by_day[day] << { name: name, duration: duration_minutes, day: "Dia #{day}", start_time: current_time_by_day[day][:time] }
-          current_time_by_day[day][:time] += duration_minutes * 60
-        else
-          organized_talks_by_day[day] << { name: 'Almoço', duration: 60, day: "Dia #{day}", start_time: Time.new(Time.now.year, Time.now.month, Time.now.day, 12, 0) }
-          current_time_by_day[day][:time] = Time.new(Time.now.year, Time.now.month, Time.now.day, 13, 0)
-          organized_talks_by_day[day] << { name: name, duration: duration_minutes, day: "Dia #{day}", start_time: current_time_by_day[day][:time] }
-          current_time_by_day[day][:time] += duration_minutes * 60
+        if current_time <= Time.new(Time.now.year, Time.now.month, Time.now.day, 12, 0)
+          if current_time + duration_minutes * 60 <= Time.new(Time.now.year, Time.now.month, Time.now.day, 12, 0)
+            organized_talks << { name: name, duration: duration_minutes, day: "#{day}", start_time: current_time }
+            current_time += duration_minutes * 60
+          else
+            # Adiciona o almoço
+            organized_talks << { name: 'Almoço', duration: 60, day: "#{day}",
+                                       start_time: Time.new(Time.now.year, Time.now.month, Time.now.day, 12, 0) }
+            current_time = Time.new(Time.now.year, Time.now.month, Time.now.day, 13, 0) # Inicia a tarde às 13h
+            # Adiciona a palestra após o almoço
+            organized_talks << { name: name, duration: duration_minutes, day: "#{day}", start_time: current_time}
+            current_time += duration_minutes * 60
+          end
+        elsif current_time < Time.new(Time.now.year, Time.now.month, Time.now.day, 17, 0)
+          # Verifica se a próxima palestra cabe no tempo restante do dia atual antes das 17h
+          if current_time + duration_minutes * 60 <= Time.new(Time.now.year, Time.now.month, Time.now.day, 17, 0)
+            organized_talks << { name: name, duration: duration_minutes, day: "#{day}", start_time: current_time }
+            current_time += duration_minutes * 60
+            if last_line
+              organized_talks << { name: 'Evento de Networking', duration: 60, day: "#{day}", start_time: current_time }
+            end
+          else
+            # Adiciona o evento de networking
+            organized_talks << { name: 'Evento de Networking', duration: 60, day: "#{day}", start_time: current_time }
+            # Adiciona palestra no dia seguinte
+            day.next!
+            current_time = Time.new(Time.now.year, Time.now.month, Time.now.day, 9, 0) # Começa às 9h no Dia A
+            organized_talks << { name: name, duration: duration_minutes, day: "#{day}", start_time: current_time }
+            current_time += duration_minutes * 60
+          end
         end
 
-        # Adiciona evento de networking se for a última linha
-        if last_line
-          day = current_time_by_day.keys.find { |d| current_time_by_day[d][:time] < Time.new(Time.now.year, Time.now.month, Time.now.day, 17, 0) }
-          organized_talks_by_day[day] << { name: 'Evento de Networking', duration: 60, day: "Dia #{day}", start_time: current_time_by_day[day][:time] }
-        end
       else
         # Lidar com linhas que não correspondem ao padrão esperado
         # Por exemplo, você pode registrar essas linhas em um arquivo de log ou tratá-las de outra forma adequada
       end
     end
 
-    organized_talks_by_day.values.flatten
+    organized_talks
   end
-
-  def initialize_current_times
-    { time: Time.new(Time.now.year, Time.now.month, Time.now.day, 9, 0), letter: 'A' }
-  end
-
-  def next_day(day)
-    (day.ord + 1).chr
-  end
-
 end
