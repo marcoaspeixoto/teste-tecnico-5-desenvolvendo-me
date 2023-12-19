56a3ef2761051ca9aba62dc2cbf93172cd45ec49
Marco Peixoto <marcoaspeixoto@gmail.com>
2023-12-18 21:24:07 -0300

    feat(talk): success response messages in html and json

--- app/controllers/talks_controller.rb
+++ app/controllers/talks_controller.rb
@@ -16,39 +16,36 @@ class TalksController < ApplicationController
 
   def home
     @import_success_message = flash[:import_success_message]
+    #flash[:import_success_message] = nil # Limpar a mensagem para evitar que ela persista após uma atualização da página
   end
 
   def import
     # Verifica se o parâmetro 'file' está presente na requisição
     unless params[:file].present? && params[:file].respond_to?(:read)
-      respond_to do |format|
-        format.html do
-          render plain: 'Arquivo não encontrado na requisição', status: :bad_request
-        end
-        format.json do
-          render json: { error: 'Arquivo não encontrado na requisição' }, status: :bad_request
-        end
-      end
-      return
+      render plain: 'Arquivo não encontrado na requisição', status: :bad_request and return
     end
 
     file_data = params[:file].read.encode('UTF-8', 'ISO-8859-1') # Lê o conteúdo do arquivo da requisição
     organized_talks = Business.organize_talks(file_data)
 
-    Talk.destroy_all # Limpa todas as palestras existentes
+    Talk.transaction do
+      Talk.destroy_all # Limpa todas as palestras existentes
 
-    organized_talks.each do |talk_data|
-      Talk.create(talk_data)
+      organized_talks.each do |talk_data|
+        Talk.create(talk_data)
+      end
     end
 
     flash[:import_success_message] = 'Palestras importadas e organizadas com sucesso'
 
     respond_to do |format|
       format.html do
+        flash[:import_success_message] = 'Palestras importadas e organizadas com sucesso'
         redirect_to root_path
       end
+
       format.json do
-        render json: { message: 'Palestras importadas e organizadas com sucesso' }
+        render json: { message: 'Palestras importadas e organizadas com sucesso' }, status: :ok
       end
     end
   end
